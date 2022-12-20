4ef08de3c81bf60081c32ec71d8379966f0e8c2c
adilzhexenov <adilzhexenoff@gmail.com>
2022-12-20 22:42:34 +0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass')(require('sass'));
+const gulpSourcemaps = require('gulp-sourcemaps');
+const gulpWatch = require('gulp-watch');
+
+gulp.task('styles-compile', () => {
+     return gulp.src('./app/scss/*.scss')
+     .pipe(gulpSourcemaps.init())
+     .pipe(gulpSass().on('error', gulpSass.logError))
+     .pipe(gulpSourcemaps.write('./'))
+     .pipe(gulp.dest('./dist/css/'));
+});
+
+
+gulp.task('watch', () => {
+     gulp.watch('./app/scss/*.scss', gulp.series('styles-compile'));
+});
